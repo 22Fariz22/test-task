bcf474a5d881d33f2f3f62e1bcfe155f882ceb15
22fariz22 <fariz08@gmail.com>
2023-10-27 15:24:03 +0300

    update

--- migrations/20231026194244_create_person_table.down.sql
+++ migrations/20231026194244_create_person_table.down.sql
@@ -1,10 +1,1 @@
-CREATE table persons
-(
-  user_id UUID PRIMARY KEY,
-  first_name  VARCHAR(30) NOT NULL ,
-  surname VARCHAR(30) NOT NULL ,
-  patronymic VARCHAR(30),
-  age INTEGER,
-  gender VARCHAR(10),
-  nationality VARCHAR(30)
-);
+DROP TABLE IF EXISTS persons;
--- migrations/20231026194244_create_person_table.up.sql
+++ migrations/20231026194244_create_person_table.up.sql
@@ -1,1 +1,10 @@
-DROP TABLE IF EXISTS persons;
+CREATE table persons
+(
+  user_id UUID PRIMARY KEY,
+  first_name  VARCHAR(30) NOT NULL ,
+  surname VARCHAR(30) NOT NULL ,
+  patronymic VARCHAR(30),
+  age INTEGER,
+  gender VARCHAR(10),
+  nationality VARCHAR(30)
+);
